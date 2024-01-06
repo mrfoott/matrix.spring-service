b80cc36c4d1c43e719b11b6ce273ea236d303492
mrfoott <phuvo2105@gmail.com>
2024-01-06 16:59:03 +0700

    Initialize repo for Matrix's Spring service

---
+++ src/test/java/matrix/spring/springservice/SpringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package matrix.spring.springservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
