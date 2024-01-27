ba0ea38e3b4bbc65a40897397a908a43aef05c1f
mrfoott <phuvo2105@gmail.com>
2024-01-27 20:44:38 +0700

    getAllOrders function

--- src/main/java/matrix/spring/springservice/controller/OrderController.java
+++ src/main/java/matrix/spring/springservice/controller/OrderController.java
@@ -2,7 +2,12 @@ package matrix.spring.springservice.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import matrix.spring.springservice.models.OrderDTO;
+import matrix.spring.springservice.services.OrderService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -11,5 +16,12 @@ public class OrderController {
 
     private final String ORDER_PATH = "/api/v1/order";
     private final String ORDER_ID_PATH = ORDER_PATH + "/{orderId}";
+
+    private final OrderService orderService;
+
+    @GetMapping
+    public List<OrderDTO> getAllOrders() {
+        return orderService.getAllOrders();
+    }
 
 }
