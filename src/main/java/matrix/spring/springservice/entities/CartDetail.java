b178f16bd70af8d3327c12c16e49ed2d7aa4b827
mrfoott <phuvo2105@gmail.com>
2024-02-23 23:59:26 +0700

    nothing new

--- src/main/java/matrix/spring/springservice/entities/CartDetail.java
+++ src/main/java/matrix/spring/springservice/entities/CartDetail.java
@@ -1,6 +1,7 @@
 package matrix.spring.springservice.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.*;
@@ -34,6 +35,10 @@ public class CartDetail {
 
 //    private UUID userId;
 //    private UUID productId;
+
+    @NotNull
+    @NotBlank
+    private String productName;
 
     @NotNull
     @PositiveOrZero
